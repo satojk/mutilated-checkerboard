efe48be152da6d39494659cdf8bb31229cc5031b
Lucas Sato <lucasjks@gmail.com>
2021-01-22 11:25:24 -0300

    Clean and prettify login page

--- login/src/login.js
+++ login/src/login.js
@@ -1,68 +1,20 @@
 import React from 'react';
 
-/*
 import './login.css';
-
-function CredentialBox(props) {
-  let inputType = props.isPassword ? 'password' : 'text'
-  return (
-    <input className=className value={props.value} onChange={props.handleChange} />
-  )
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: null,
-      password: null,
-    }
-  }
-
-  updateUsername(newUsername) {
-    this.setState({
-      username: newUsername,
-      password: this.state.password,
-    })
-  }
-
-  updatePassword(newPassword) {
-    this.setState({
-      username: this.state.username,
-      password: newPassword,
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Login</h1>
-        <CredentialBox
-          value={this.state.username}
-          isPassword={false}
-          handleChange={(event) => this.updateUsername(event.target.value)}
-        />
-        <CredentialBox
-          value={this.state.password}
-          isPassword={true}
-          handleChange{(event) => this.updatePassword(event.target.value)}
-        />
-      </div>
-    )
-  }
-}
-*/
 
 class App extends React.Component {
   render() {
     return(
-      <form action='/login' method='post' id='login'>
-        <label for='username'>Username:</label><br />
-        <input type='text' id='username' name='username' /><br />
-        <label for='password'>Password:</label><br />
-        <input type='password' id='password' name='password' /><br /><br />
-        <input type='submit' value='Log in' />
-      </form>
+      <div className='login-div'>
+        <p className='instructions'>Please log into the experiment using the credentials given to you. You may only log in once, so once logged in, do not quit or close the window until finished. If you believe something is wrong, or if there are any issues, contact <a href='mailto:satojk@stanford.edu'>satojk@stanford.edu</a></p>
+        <form className='login-form' action='/login' method='post' id='login'>
+          <label for='username'>Username:</label><br />
+          <input type='text' id='username' name='username' /><br />
+          <label for='password'>Password:</label><br />
+          <input type='password' id='password' name='password' /><br /><br />
+          <input type='submit' value='Log in' />
+        </form>
+      </div>
     )
   }
 }
