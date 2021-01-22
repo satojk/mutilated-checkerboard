e55c52e755b8afa799ed0568aed9e469aa1f6d00
Lucas Sato <lucasjks@gmail.com>
2021-01-21 23:28:58 -0300

    Make Main menu reflect progress through experiment
    Now only the next stage to be completed is linked in the main screen,
    and completed stages are marked as such.
    
    A user could still type in the url though, so this should be explicitly
    disallowed on the GET route, server side.

--- main/src/main.js
+++ main/src/main.js
@@ -27,17 +27,40 @@ class App extends React.Component {
     let toReturn;
     if (this.state.loading) {
       toReturn = <p>Page loading...</p>;
-    }
-    else {
+    } else {
+      let stage1 = <p><a href='/stage1'>Stage 1</a> (30 minutes)</p>;
+      if (this.state.sessionCompleted > 0) {
+        stage1 = <p>Stage 1 (30 minutes) - COMPLETED</p>;
+      }
+
+      let stage2 = <p>Stage 2 (15 minutes)</p>;
+      if (this.state.sessionCompleted === 1) {
+        stage2 = <p><a href='/stage2'>Stage 2</a> (15 minutes)</p>;
+      }
+      if (this.state.sessionCompleted > 1) {
+        stage2 = <p>Stage 2 (15 minutes) - COMPLETED</p>;
+      }
+
+      let stage3 = <p>Stage 3 (15 minutes)</p>;
+      let concludingMessage;
+      if (this.state.sessionCompleted === 2) {
+        stage3 = <p><a href='/stage3'>Stage 3</a> (15 minutes)</p>;
+      }
+      if (this.state.sessionCompleted > 2) {
+        stage3 = <p>Stage 3 (15 minutes) - COMPLETED</p>;
+        concludingMessage = <p>You have completed all stages of the experiment. Thank you very much! You may now close this window.</p>;
+      }
+
       toReturn = (
         <div>
           <p>Hello, {this.state.sessionName}.</p>
           <p>The experiment has three distinct stages. Please click on and complete each of the three following stages in the order shown.</p>
           <ol>
-            <li><a href='/stage1'>Stage 1</a> (30 minutes)</li>
-            <li><a href='/stage2'>Stage 2</a> (15 minutes)</li>
-            <li><a href='/stage3'>Stage 3</a> (15 minutes)</li>
+            <li>{stage1}</li>
+            <li>{stage2}</li>
+            <li>{stage3}</li>
           </ol>
+          {concludingMessage}
         </div>
       );
     }
