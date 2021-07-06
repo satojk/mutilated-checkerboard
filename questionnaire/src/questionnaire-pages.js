13854bcbb8a11a81e1b992917a3a26c5da760fc2
Lucas Sato <lucasjks@gmail.com>
2021-07-06 08:16:32 -0700

    Make minor changes in wording to questionnaire

--- questionnaire/src/questionnaire-pages.js
+++ questionnaire/src/questionnaire-pages.js
@@ -41,8 +41,8 @@ export class QuestionnairePage1 extends React.Component {
 
   render() {
     const prompts = [
-      'Think back to the very beginning when you were first introduced to the puzzle. What did you think and do in the first few minutes?',
-      'In the first few minutes of the puzzle, what were the things you initially noticed about the puzzle and its components? List all observations you recall making initially.',
+      'Think back to the very beginning when you were first introduced to the puzzle. What did you think and do in the first two minutes?',
+      'In the first two minutes of the puzzle, what were the things you initially noticed about the puzzle and its components? List all observations you recall making initially.',
       'What approaches did you take initially, and why? List all lines of thought that you recall pursuing (in chronological order), alongside how long you pursued each of them, to the best of your recollection. Include observations which may have prompted these lines of thought, as well as observations made as you pursued these lines of thought.',
     ];
     let maySubmit = this.state.responses.reduce(
@@ -213,9 +213,9 @@ export class QuestionnairePage3 extends React.Component {
     super(props);
     let q1Options = [
       '"It would be possible and easy to cover the board if the two removed squares were of non-diagonally-opposite corners (e.g. top right and bottom right)."',
-      '"A domino must cover, specifically, two abutting squares."',
+      '"A domino must cover two abutting squares."',
       '"There were 62 squares to cover, so you would need exactly 31 dominos."',
-      '"Adjacent squares are always of opposite colors."',
+      '"Abutting squares are always of opposite colors."',
       '"If you could cover two diagonally-adjacent blocks, the problem would be easy."',
       '"You could, in principle, try every covering possible and find the answer, but that would take too long."',
       '"Both removed squares were light-colored squares."',
@@ -266,7 +266,7 @@ export class QuestionnairePage3 extends React.Component {
     const rangeOptions = ['1', '2', '3', '4', '5']
     const rangeEndpoints = ['Strongly disagree', 'Strongly agree']
     const prompts = [
-      'Below we will present 13 possible observations about the puzzle in quotes, one at a time. For each observation, click "Yes" if you were aware of it and thought about it at some point during solving the puzzle (NOT while reading these now), or "No" if you did not think about it during solving the puzzle.',
+      'Below we will present 13 possible observations about the puzzle in quotes, one at a time. For each observation, click "Yes" if you were aware of it and thought about it at some point as you were trying to solve the puzzle (NOT while reading these now), or "No" if you did not think about it as you were trying to solve the puzzle.',
       'Please list any other observations which you attended to and thought about during the process of solving the problem. Please try to think of as many as you can and be as thorough as possible.',
       'How strongly do you agree with the following statement in quotes? "I actively attempted to make general observations about the problem like those in the list above."',
     ];
